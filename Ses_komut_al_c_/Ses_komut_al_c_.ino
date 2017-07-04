330fb20b8a6a73ab6d4b749a39fd14ace802f161
metinakkin <metin_akkin@hotmail.com>
2017-07-04 16:33:28 +0300

    Add files via upload

---
+++ Ses_komut_al_c_/Ses_komut_al_c_.ino
@@ -0,0 +1,81 @@
+#include <SPI.h>                       // SPI Arayüzünü dahil ettik.
+#include "RF24.h"                      // RF24 kütüpanelerini dahil ettik.
+byte data[1];                          // Göndereceğimiz data değişkenini oluşturduk.
+boolean var;                           // Bit şeklinde var değişkeninini oluşturduk.
+const uint64_t pipe = 0xF0F0F0F0A1LL;  // Paralel veri yolu adresleri tanımlamasını yaptık.
+RF24 radio(9,10);                      // Modülün CE pinini 9.pin CS pinini de 10.pine bağlayacağımızı belirttik.
+const int sol_i = 4;
+const int sol_g = 5;
+const int sag_i = 6;
+const int sag_g = 7;
+void setup(){
+ Serial.begin(115200);
+ radio.begin();                         // Kablosuz iletişimi başlattık.
+ radio.openReadingPipe(1,pipe);         // Veri alacağımız kanalın ID'sini tanımladık.
+ radio.startListening();  
+  pinMode(sag_i, OUTPUT);
+  pinMode(sag_g, OUTPUT);
+  pinMode(sol_i, OUTPUT);
+  pinMode(sol_g, OUTPUT);              
+}
+
+void loop()
+{
+ if(radio.available()){
+   var = false;
+   while(!var){
+     var = radio.read(data, 1);   
+      if(data[0] == 1)
+      {
+      Serial.println(data[0]);
+      digitalWrite(sag_i, HIGH);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, HIGH);
+      digitalWrite(sol_g, LOW);
+      delay(600);
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, LOW);
+      }
+      if(data[0] == 2)
+      {
+      Serial.println(data[0]);
+       digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, HIGH);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, HIGH);
+      delay(600);
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, LOW);
+      }
+      if(data[0] == 3)
+      {
+      Serial.println(data[0]);
+       digitalWrite(sag_i, HIGH);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, HIGH);
+      delay(300);
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, LOW);
+      }
+      if(data[0] == 4){
+      Serial.println(data[0]);
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, HIGH);
+      digitalWrite(sol_i, HIGH);
+      digitalWrite(sol_g, LOW);
+      delay(300);
+      digitalWrite(sag_i, LOW);
+      digitalWrite(sag_g, LOW);
+      digitalWrite(sol_i, LOW);
+      digitalWrite(sol_g, LOW);
+   }
+  }
+ }
+}
---
+++ Ses_komut_verici/Ses_komut_verici.ino
@@ -0,0 +1,125 @@
+#include  <SPI.h>                     // SPI Arayüzünü dahil ettik.
+#include  "RF24.h"                    // RF24 kütüpanelerini dahil ettik.
+#include <SoftwareSerial.h>
+#include "VoiceRecognitionV3.h"
+
+byte data[1];                         // Göndereceğimiz data değişkenini oluşturduk.
+const uint64_t pipe = 0xF0F0F0F0A1LL; // Paralel veri yolu adresleri tanımlamasını yaptık.
+uint8_t buf[64];
+
+RF24 radio(9,10);                     // Modülün CE pinini 9.pin CS pinini de 10.pine bağlayacağımızı belirttik.
+VR myVR(2,3);    // 2:RX 3:TX, you can choose your favourite pins.
+
+void setup()
+{
+ myVR.begin(9600);
+ radio.begin();                       // Kablosuz iletişimi başlattık.
+ radio.openWritingPipe(pipe);         // Gönderim yapacağımız kanalın ID'sini tanımladık.
+}
+
+void loop()
+{
+  int ret;
+  ret = myVR.recognize(buf, 50);
+  if(ret>0){
+    Serial.println(buf[1]);
+    switch(buf[1])
+    {
+      
+      case 1: // Yeşil
+      { data[0]=1;
+      break;}
+      case 2: // Mavi
+      { data[0]=2;
+      break;}
+      case 3: //Sarı
+      { data[0]=3;
+      break;}
+       case 4: // Mor
+      { data[0]=4;
+      break;}
+                
+               
+              
+       
+      default:
+        break;
+    }
+    /** voice recognized */
+   
+    
+  }
+  radio.write(data, 1);                 //Sonra da bu mesaj gönderilsin.
+}
+
+/**
+  @brief   Print signature, if the character is invisible, 
+           print hexible value instead.
+  @param   buf     --> command length
+           len     --> number of parameters
+*/
+void printSignature(uint8_t *buf, int len)
+{
+  int i;
+  for(i=0; i<len; i++){
+    if(buf[i]>0x19 && buf[i]<0x7F){
+      Serial.write(buf[i]);
+    }
+    else{
+      Serial.print("[");
+      Serial.print(buf[i], HEX);
+      Serial.print("]");
+    }
+  }
+}
+
+/**
+  @brief   Print signature, if the character is invisible, 
+           print hexible value instead.
+  @param   buf  -->  VR module return value when voice is recognized.
+             buf[0]  -->  Group mode(FF: None Group, 0x8n: User, 0x0n:System
+             buf[1]  -->  number of record which is recognized. 
+             buf[2]  -->  Recognizer index(position) value of the recognized record.
+             buf[3]  -->  Signature length
+             buf[4]~buf[n] --> Signature
+*/
+void printVR(uint8_t *buf)
+{
+  Serial.println("VR Index\tGroup\tRecordNum\tSignature");
+
+  Serial.print(buf[2], DEC);
+  Serial.print("\t\t");
+
+  if(buf[0] == 0xFF){
+    Serial.print("NONE");
+  }
+  else if(buf[0]&0x80){
+    Serial.print("UG ");
+    Serial.print(buf[0]&(~0x80), DEC);
+  }
+  else{
+    Serial.print("SG ");
+    Serial.print(buf[0], DEC);
+  }
+  Serial.print("\t");
+
+  Serial.print(buf[1], DEC);
+  Serial.print("\t\t");
+  if(buf[3]>0){
+    printSignature(buf+4, buf[3]);
+  }
+  else{
+    Serial.print("NONE");
+  }
+//  Serial.println("\r\n");
+  Serial.println();
+}
+
+void printRecord(uint8_t *buf, uint8_t len)
+{
+  Serial.print(F("Record: "));
+  for(int i=0; i<len; i++){
+    Serial.print(buf[i], DEC);
+    Serial.print(", ");
+  }
+}
